123d5b1289d4d0cbe0167fc400571783d16ee876
Anthony <anthony.bellier@laposte.net>
2022-08-18 09:19:34 +0200

    Protected heritage

--- héritage/main_protected_heritage.php
+++ héritage/main_protected_heritage.php
@@ -5,33 +5,24 @@ class Mere {
 
   protected $attribut="Bonjour";
 
-  public  function __construct() {
-    $this->attribut;
-  }
-
-  public function __destruct()
-  {
-    
-  }
-
   public function methode1() {
     $str = $this->attribut;
-    $str.=" je suis la classe Mere ";
+    $str.=" je suis la classe Mere.";
     return $str;
   }
 }
 
 class Fille extends Mere{
+
   public function methode2(){
     $str = $this->attribut;
-    $str.=" je suis la classe Fille qui hérite de la Mere ";
+    $str.=" je suis la classe Fille.";
     return $str;
   }
 }
 
 
-$objet = new Mere();
+$objet = new Fille();
 echo $objet->methode1();
 echo '<hr>';
-$objet2 = new Fille();
-echo $objet2->methode2();
+echo $objet->methode2();
