d08818b10a07268498faa31642711a5c00c38dc6
Anthony <anthony.bellier@laposte.net>
2022-08-24 12:34:05 +0200

    (docs) explications du fichier php.ini (production & developpement)

--- infophp.php
+++ infophp.php
@@ -1,3 +1,7 @@
 <?php
 
 phpinfo();
+
+// php.ini
+// fichier development "php.ini-development" activer divertes options pour le dev et on desactive certaine extensions pour la production
+// fichier prodution "php.ini-production" rename en php pour ecraser le fichier php
